d82c16c51e1c783d81ffa59357b88057018f9162
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-11-03 11:05:41 +0000

    Feat: add for directive

--- corso-angular/src/app/structural-directives/structural-directives.ts
+++ corso-angular/src/app/structural-directives/structural-directives.ts
@@ -8,10 +8,30 @@ import { Component, signal, WritableSignal } from '@angular/core';
 })
 
 export class StructuralDirectives {
+  // esempio 1
   visualizza: WritableSignal<boolean> = signal(false);
+
+  // esempio 2
+  immagineOTesto: WritableSignal<string> = signal('immagine');
+
+  // esempio 3
+  nomi: WritableSignal<string[]> = signal(['Andrea', 'Gianni', 'Federico', 'Luca']);
 
   toggleVisualizza(): void {
     this.visualizza.update(valoreCurrent => !valoreCurrent); // espressione lambda
+  }
+
+  vaiAImmagine(): void {
+    this.immagineOTesto.set('immagine');
+  }
+
+  vaiATesto(): void {
+    this.immagineOTesto.set('testo');
+  }
+
+  aggiungiNome(nome: any): void {
+    console.log(nome.value);
+
   }
 
 
