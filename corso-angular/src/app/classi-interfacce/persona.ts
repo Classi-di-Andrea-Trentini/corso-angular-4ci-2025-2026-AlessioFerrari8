e1e0ad7f6a75dc09c1135b2158cb96d04ea89af4
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-11-10 10:53:40 +0000

    Feat: create class Persona, with attributes, constructor, getters and setters

---
+++ corso-angular/src/app/classi-interfacce/persona.ts
@@ -0,0 +1,55 @@
+export class Persona {
+  // Attributi della classe
+  private _id: number; // inizializzo o a 0, o metto subito nel costruttore
+  private _nome: string;
+  private _cognome: string;
+  private _sesso: string;
+
+  constructor(id: number, nome: string, cognome: string, sesso: string) {
+    this._id = id;
+    this._nome = nome;
+    this._cognome = cognome;
+    this._sesso = sesso;
+  }
+
+  // getter
+  public get id(): number {
+    return this._id;
+  }
+
+  public get nome(): string {
+    return this._nome;
+  }
+
+  public get cognome(): string {
+    return this._cognome;
+  }
+
+  public get sesso(): string {
+    return this._sesso;
+  }
+
+  public get nomeCompleto(): string {
+    return this._nome + ' ' + this._cognome;
+  }
+
+  // setter
+
+  public set nome(value: string) {
+    this._nome = value;
+  }
+
+  public set cognome(value: string) {
+    this._cognome = value;
+  }
+
+  public set sesso(value: string) {
+    this._sesso = value;
+  }
+
+  // metodi della classe
+  public toString(): string {
+    return this._id + ' ' + this._nome + ' ' + this._cognome;
+  }
+
+}
